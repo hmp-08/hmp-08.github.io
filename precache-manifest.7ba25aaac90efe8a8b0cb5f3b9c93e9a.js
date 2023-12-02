9d92f94e5074cf248767552ec409df49677f1c8a
hmp-08 <haymarphyoe08@gmail.com>
2023-12-02 10:47:03 +0630

    Updates

---
+++ precache-manifest.7ba25aaac90efe8a8b0cb5f3b9c93e9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33501346880d295e11f41ce9798b0f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3aad5e50839781c2404",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "02ded95b2759484aa1a3",
+    "url": "/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "b3aad5e50839781c2404",
+    "url": "/static/js/2.8654c17f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.8654c17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ded95b2759484aa1a3",
+    "url": "/static/js/main.7ccaf01c.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
